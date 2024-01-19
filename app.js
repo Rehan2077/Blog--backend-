a01eb49e1cf5c8d0942ea3ac5a4cbe5516dd4a49
Rehan2077 <rfazal0124@gmail.com>
2024-01-20 00:33:23 +0530

    upload folder added to git

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ import { errorMiddleware, invalidPathHandler } from "./middleware/error.js";
 
 export const app = express();
 app.set("trust proxy", 1);
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+}));
 
 app.use(express.json());
 app.use(cookieParser());
